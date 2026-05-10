aae9c30816180d86f98b5dbac821e1ee1bf6c570
Bryan Gregory <BryanGregoryTX@outlook.com>
2026-05-10 16:50:00 -0500

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,4 +1,17 @@
+** Carrier Performance **
 SELECT
+	c.carrier_name,
+	COUNT(*) AS total_shipments,
+	SUM(CASE WHEN s.delivery_status = 'Delayed' THEN 1 ELSE 0 END) AS delayed_shipments,
+	ROUND(AVG(s.shipping_cost), 2) AS avg_shipping_cost
+FROM shipments s
+JOIN carriers c
+	ON s.carrier_id = c.carrier_id
+GROUP BY c.carrier_name
+ORDER BY delayed_shipments DESC, avg_shipping_cost DESC;
+**********************************************************
+** Warehouse Performance **
+	SELECT
 	w.warehouse_name,
 	COUNT(*) AS total_shipments,
 	SUM(CASE WHEN s.delivery_status = 'Arrived' THEN 1 ELSE 0 END) AS arrived_shipments,
@@ -12,7 +25,7 @@ JOIN warehouses w
 GROUP BY w.warehouse_name
 ORDER BY on_time_rate DESC;
 *******************************************************
-
+** Shipments On-Time vs Delayed **
 SELECT
 	delivery_status,
 	COUNT (*) AS shipment_count
@@ -20,7 +33,7 @@ FROM shipments
 GROUP BY delivery_status
 ORDER BY shipment_count DESC;
 *********************************************************
-
+** Shipping Cost by Region **
  SELECT
  	customer_region, 
 	 COUNT(*) AS total_shipments,
@@ -29,6 +42,7 @@ FROM shipments
 GROUP BY customer_region
 ORDER BY avg_shipping_cost DESC;
 **********************************************************
+** Revenue by Category **
 SELECT
 	p.category,
 	COUNT(*) AS total_orders,
@@ -39,7 +53,7 @@ JOIN products_summary p
 GROUP BY p.category
 ORDER BY total_order_value DESC;
 ***********************************************************
-
+** On-Time Delivery Rate % **
 SELECT
 	ROUND(
 	100.0 * SUM(CASE WHEN delivery_status = 'Arrived' THEN 1 ELSE 0 END) / COUNT(*),
@@ -47,7 +61,7 @@ SELECT
 ) AS on_time_delivery_rate
 FROM shipments;
 ************************************************************
-
+** Full Business Summary **
 SELECT
 	s.order_id,
 	c.carrier_name,
@@ -70,7 +84,7 @@ JOIN products_summary p
 	ON s.order_id = p.order_id
 ORDER BY s.order_id;
 ***************************************************************
-
+** Delays by Region **
 SELECT
 	customer_region,
 	COUNT (*) AS total_shipments,
@@ -79,7 +93,7 @@ FROM shipments
 GROUP BY customer_region
 ORDER BY delayed_shipments DESC;
 ****************************************************************
-
+** Shipping Cost by Category **
 SELECT
 	p.category,
 	COUNT(*) AS total_orders,
@@ -90,8 +104,7 @@ JOIN products_summary p
 GROUP BY p.category
 ORDER BY avg_shipping_cost DESC;
 ***************************************************************
-
-**Average Delivery Days
+** Average Delivery Days **
 SELECT
 	c.carrier_name,
 	ROUND(AVG(actual_delivery_date - ship_date), 2) AS avg_delivery_days
